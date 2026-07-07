e301c783cf71f3ba9a1dd3f91f9a24c1b4fac91a
Nama Anda <email_anda@example.com>
2026-07-07 08:27:47 +0800

    update url dan key supabase

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // Ganti dengan kredensial proyek Supabase Anda (Settings → API).
-export const SUPABASE_URL = "https://hyuikurbbeuoazxozobq.co";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh5dWlrdXJiYmV1b2F6eG96b2JxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMzNjc0MzgsImV4cCI6MjA5ODk0MzQzOH0.kKcufIvB0tK7ReRh_wZzpPSqz7TAWWXGF9dkryYNVCo";
+export const SUPABASE_URL = "https://hyuikurbbeuoazxozobq.supabase.co";
+export const SUPABASE_ANON_KEY = "sb_publishable_OttCzxrWi0HeRbZf_l6gxQ_GaU0dW0C";
